c4b16e71fe518a2f621f792029460c85f6f63cbc
Aparna <tiwari.aparna2899@gmail.com>
2022-06-21 22:48:26 +0530

    Mongoose Introduction

---
+++ block-BNaabz/app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017', (err) => {
+  console.log(err ? err : 'Connected to Database!');
+});
+
+var app = express();
+
+app.listen(4000, () => {
+  console.log('server is listening on port 4000');
+});
